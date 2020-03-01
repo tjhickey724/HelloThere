fbc40853f70b944e6b529c4646692d558dd808f7
tim <tjhickey@brandeis.edu>
2020-03-01 18:58:18 -0500

    added another println

--- FirstProject/src/cs12bspr20/HelloThere.java
+++ FirstProject/src/cs12bspr20/HelloThere.java
@@ -5,6 +5,7 @@ public class HelloThere {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello there!");
+		System.out.println("How is it going?");
 
 	}
 
