34805e041ff50e5a245f2e89776675f680a4f741
tim <tjhickey@brandeis.edu>
2020-03-01 18:56:54 -0500

    first commit!

---
+++ FirstProject/src/cs12bspr20/HelloThere.java
@@ -0,0 +1,11 @@
+package cs12bspr20;
+
+public class HelloThere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello there!");
+
+	}
+
+}
---
+++ FirstProject/src/cs12bspr20/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This is a demo package for the CS12b course in Spring 2020
+ */
+/**
+ * @author tim
+ *
+ */
+package cs12bspr20;
