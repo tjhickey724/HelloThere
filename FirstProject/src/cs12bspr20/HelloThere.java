08365cb607d792b36f3033bd655a3a554ca7ed02
tim <tjhickey@brandeis.edu>
2020-03-01 19:01:32 -0500

    minor change

--- FirstProject/src/cs12bspr20/HelloThere.java
+++ FirstProject/src/cs12bspr20/HelloThere.java
@@ -5,7 +5,7 @@ public class HelloThere {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello there!");
-		System.out.println("How is it going?");
+		System.out.println("How is it going?"); 
 
 	}
 
